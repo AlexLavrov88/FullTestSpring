3e8b332eba158cf0de8e9458c41ef736d5bb9a7e
Alex Lavrov <lavrov_alex@list.ru>
2023-10-18 21:01:02 +0300

    First commit.

---
+++ src/main/java/lavr/fulltestsprimg/FullTestSprimgApplication.java
@@ -0,0 +1,13 @@
+package lavr.fulltestsprimg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullTestSprimgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullTestSprimgApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lavr/fulltestsprimg/FullTestSprimgApplicationTests.java
@@ -0,0 +1,13 @@
+package lavr.fulltestsprimg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullTestSprimgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
